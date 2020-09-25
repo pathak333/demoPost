fe0280852526821cdc254b1e1dc705c3ba2bee49
abhay pathak <pathakabhay31@gmail.com>
2020-09-25 11:19:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+import { RestService } from './rest.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'post';
+  posts: Object;
+  name = ['Ajay', 'Bunty', 'Chintan', 'Darshan', 'Rakesh', 'Mayank', 'Ramesh', 'Suresh', 'Jay', 'Prem']
+  constructor(private rest: RestService) { }
+
+  ngOnInit() {
+    AOS.init();
+    try {
+      var datas = this.rest.get('https://jsonplaceholder.typicode.com/posts').then(data => {
+        console.log(data);
+        this.posts = data;
+      })
+
+    } catch (err) { }
+
+  }
+
 }
